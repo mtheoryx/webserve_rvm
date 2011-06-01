16dc3e0d141efa87a8c497db881e03b0ea7e09fd
David Poindexter <davpoind@iupui.edu>
2011-06-01 14:55:43 -0400

    Using absolute paths.

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Remote exectution test from github
-# echo $HOME
-
-# Execute different command to test git push remote for public file
-# echo $PATH
-
-
-
 # Trying a client side post commit, instead of another remote added
 `whoami`
