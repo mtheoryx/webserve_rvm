cd9590f4d3eb54fd1ff84ab11c58dcb844841138
David Poindexter <davpoind@iupui.edu>
2011-06-01 15:12:00 -0400

    Grabbing latest git source.

--- initialize.sh
+++ initialize.sh
@@ -1,11 +1,23 @@
 #!/bin/env bash
 
-abort=false
-
 # First, see if rvm or git is already installed for this user
 if command -v git
 then
     printf "Git already installed.\n"
 else
     printf "Installing Git for user (git command not found)"
+    src_path=$HOME/.src
+    dest_path=$HOME/opt
+    
+    package=git
+    version=1.7.5.3
+    
+    mkdir -p $src_path
+    mkdir -p $dest_path
+    
+    cd $src_path
+    
+    curl -LO http://kernel.org/pub/software/scm/git/$package-$version.tar.bz2
+    tar -xjvf $package-$version.tar.bz2
+    
 fi
