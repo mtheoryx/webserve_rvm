6104933d6b9fd512f96513ebd827c7c354e0cda3
David Poindexter <davpoind@iupui.edu>
2011-06-01 14:52:59 -0400

    Testing another hook, but with no additional remote receive.

--- initialize.sh
+++ initialize.sh
@@ -6,5 +6,5 @@
 # Execute different command to test git push remote for public file
 # echo $PATH
 
-# Trying a client side post commit, instead
+# Trying a client side post commit, instead of another remote added
 `whoami`
