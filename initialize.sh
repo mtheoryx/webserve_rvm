4bce9312cae1a78e0928faab1395d7b9ba6234bf
David Poindexter <davpoind@iupui.edu>
2011-06-01 15:03:20 -0400

    Git detection test on webserve known not to have git installed.

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# Trying a client side post commit, instead of another remote added
-echo `whoami`
+abort = false
+
+# First, see if rvm or git is already installed for this user
+if command -v git
+then
+    printf "Git already installed.\n"
+else
+    printf "Installing Git for user (git command not found)"
+fi
