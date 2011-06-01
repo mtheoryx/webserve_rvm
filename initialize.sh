8cb44faf6d5f4ff25c60dcfa09df2f472317a9d9
David Poindexter <davpoind@iupui.edu>
2011-06-01 16:26:17 -0400

    First rvm install test file.

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,12 @@
 #!/bin/env bash
 
+git=false
+
 # First, see if rvm or git is already installed for this user
 if command -v git
 then
     printf "Git already installed.\n"
+    git=true
 else
     printf "Installing Git for user (git command not found)\n"
     src_path=$HOME/.src
@@ -35,6 +38,20 @@ else
     cd $dest_path
     
     printf "Git installation complete. Setting up your path...\n"
+    touch $HOME/.bash_profile
+    
+    # temporarily doing this until i can figure out the path problems on webserve
     ln -s $dest_path/bin/git $HOME/bin/git
     
+    if command -v git
+    then
+        git=true
+    else
+        git=false
+    fi
 fi
+printf "=> Installing RVM the Ruby enVironment Manager\n  https://rvm.beginrescueend.com/rvm/install/\n"
+# TODO: Include the rvm installer in this script
+# TODO: Include initial rvm rubies in this script
+# TODO: Include initial gemset creation and configuration in this script
+# TODO: install base level ruby gems in this script
